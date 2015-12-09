c793ba366d3922ffb5feff8cab497496da529915
Abhinav Mishra <abhinav.mishra15@imperial.ac.uk>
2015-12-09 18:15:44 +0000

    Implemented Max and Min functios.

---
+++ MiniMax.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+
+public class MiniMax extends MoveGenAI2{
+
+	private double INF = 10000;
+	private Move choice;
+	public MiniMax(Game game, Board board, Player self, Player opponent) {
+		super(game, board, self, opponent);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void minimax(int depth) {
+		
+	}
+	
+	private double Max(int depth) {
+		
+		if (depth == 0)
+			return evaluateBoard(self, opponent);
+		
+		double best = -INF;
+		ArrayList<Move> moves = self.listOfValidMoves();
+		while (moves.size() > 0) {
+			Move tempChoice = moves.remove(0);
+			board.applyMove(tempChoice);
+			double val = -Min(depth-1);
+			if (val > best) {
+				best = val;
+				choice = tempChoice;
+			}
+			board.unapplyMove(tempChoice);
+		}
+		return best;
+	}
+	
+	private double Min(int depth) {
+		
+		if (depth == 0)
+			return evaluateBoard(self, opponent);
+		
+		double best = -INF;
+		ArrayList<Move> moves = self.listOfValidMoves();
+		while (moves.size() > 0) {
+			Move tempChoice = moves.remove(0);
+			board.applyMove(tempChoice);
+			double val = -Max(depth-1);
+			if (val > best) {
+				best = val;
+				choice = tempChoice;
+			}
+			board.unapplyMove(tempChoice);
+		}
+		return best;
+	}
+}
